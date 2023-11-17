c0aed2b13ff07495df261cdf7047fc1e6ce49135
Artem Gilmanov <84340745+artemgilmanov@users.noreply.github.com>
2023-11-17 20:27:23 +0100

    Add files via upload

---
+++ Basics/2_Operators/2_1 Concantinate/main.js
@@ -0,0 +1,19 @@
+"use strict";
+
+let java = "Java";
+let script = "Script";
+let test = 100;
+
+let javaScriptTest = java + script + " " + test + "%";
+
+console.log(javaScriptTest + "\n\t moved to new line and with tab!");
+
+let firstName = "FirstName";
+let secondName="SecondName";
+let age = 18;
+
+console.log(`${firstName} ${secondName} ${age}`);
+
+console.log(`${firstName} 
+${secondName} 
+${age}`);
