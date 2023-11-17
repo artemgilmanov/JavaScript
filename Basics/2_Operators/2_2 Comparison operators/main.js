34f430aa788a32f42211a759d4431cbf1ad78244
Artem Gilmanov <84340745+artemgilmanov@users.noreply.github.com>
2023-11-17 20:27:53 +0100

    Add files via upload

---
+++ Basics/2_Operators/2_2 Comparison operators/main.js
@@ -0,0 +1,32 @@
+"use strict";
+
+console.log(42 == 42);
+console.log(42 == 43);
+console.log("--------");
+
+console.log(42 != 42);
+console.log(42 > 42);
+console.log(42 >= 42);
+console.log("--------");
+
+console.log("K" == "k");
+console.log("K" < "k");
+console.log("--------");
+
+console.log("test" == "test");
+console.log("test" == "Test");
+console.log("--------");
+
+let myVariableOne = "myVarial";
+let myVariableTwo = 18;
+
+let myVariableThree = "15";
+//myVariableThree = 15;
+let myVariableFour = 5;
+
+console.log(myVariableThree+myVariableFour);
+console.log("--------");
+
+console.log("42" == 42);
+console.log("42" === 42);
+console.log("42" !== 42);
---
+++ Basics/2_Operators/2_3 Logical operators/main.js
@@ -0,0 +1,10 @@
+"use strict";
+
+console.log(1>0 && 4<6);
+console.log(1>0 && 6<4);
+
+console.log(1>0 || 4<6);
+console.log(1>0 || 6<4);
+
+console.log(1>0 && !(4<6));
+console.log(!true);
