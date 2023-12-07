d316408f99420ebdf2c95072e727fd089b91ae31
Artem Gilmanov <84340745+artemgilmanov@users.noreply.github.com>
2023-12-07 11:51:21 +0100

    Add files via upload

--- Basics/8_Loops/forEach.js
+++ Basics/8_Loops/forEach.js
@@ -1,12 +1,23 @@
 'use strict';
 
-let zahlen = [5, 56, 47, 8, 49, 35, 3, 67, 90];
-console.log(zahlen);
-/*
-let callback_funktion = (e) => {
-  console.log(e);
-};
-*/
-zahlen.forEach(function (e) {
-  console.log(e);
+let colors = ['red', 'blue', 'green'];
+colors.forEach(function (color) {
+  console.log(color);
 });
+
+let zahlen = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let sum = 0;
+console.log(zahlen);
+
+function adder(number) {
+  sum += number;
+}
+
+/*
+numbers.forEach(function(number){
+  sum += number;
+})
+*/
+
+zahlen.forEach(adder);
+console.log(sum);
