2cce130a01ac103b17ecff5beb18f6ca6baf6aa5
Artem Gilmanov <84340745+artemgilmanov@users.noreply.github.com>
2024-01-24 07:45:25 +0100

    Add files via upload

---
+++ Basics/16_Rounding/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+console.log(14.23 + 354.97);
+console.log(1047.465 + 354.97);
+console.log(99.999 + 354.97);
+// work around
+console.log(((99.999 * 100 + 354.97 * 100) / 100).toFixed(2));
+
+console.log(93332654729891549);
