cc577facf101978a4a17c94240ff019c9eea4aad
Artem Gilmanov <84340745+artemgilmanov@users.noreply.github.com>
2024-02-06 07:20:44 +0100

    Add files via upload

---
+++ Dom/0_Properties and Methods/1_GetElement/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+let el_by_id = document.getElementById('navigation');
+console.log(el_by_id);
+
+let els_by_class_name = document.getElementsByClassName('jumbotron');
+console.log(els_by_class_name);
+
+let els_by_tah_name = document.getElementsByTagName('p');
+console.log(els_by_tah_name);
