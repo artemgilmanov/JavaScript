a4a8f97dcb4a1ac70880c5412621d6a95acde7d9
Artem Gilmanov <84340745+artemgilmanov@users.noreply.github.com>
2024-02-09 22:17:29 +0100

    Add files via upload

---
+++ Dom/2_QuerySelector/main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+let qs_1 = document.querySelector('body > nav > ul');
+console.log(qs_1);
+let qs_2 = document.querySelector('.jumbotron');
+console.log(qs_2);
+let qs_3 = document.querySelectorAll('body > nav > ul');
+console.log(qs_3);
